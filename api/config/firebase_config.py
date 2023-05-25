57e3f0dcce3b3b85099a9f039d9a176c6e345a9a
ArturoJCastilloZ <castillo.arturo93@hotmail.com>
2023-05-25 10:39:06 -0600

    Actions github

--- api/config/firebase_config.py
+++ api/config/firebase_config.py
@@ -10,14 +10,14 @@ BLOB_NAME = os.getenv("BLOB")
 BUCKET_NAME = os.getenv("BUCKET")
 
 CREDENTIAL_JSON = ({
-    "TYPE": os.getenv("TYPE"),
-    "PROJECT_ID": os.getenv("PROJECT_ID"),
-    "PRIVATE_KEY_ID": os.getenv("PRIVATE_KEY_ID"),
-    "PRIVATE_KEY": os.getenv("PRIVATE_KEY"),
-    "CLIENT_EMAIL": os.getenv("CLIENT_EMAIL"),
-    "CLIENT_ID": os.getenv("CLIENT_ID"),
-    "AUTH_URI": os.getenv("AUTH_URI"),
-    "TOKEN_URI": os.getenv("TOKEN_URI"),
-    "AUTH_PROVIDER_X509_CERT_URL": os.getenv("AUTH_PROVIDER_X509_CERT_URL"),
-    "CLIENT_X509_CERT_URL": os.getenv("CLIENT_X509_CERT_URL")
+    "type": os.getenv("TYPE"),
+    "project_id": os.getenv("PROJECT_ID"),
+    "private_key_id": os.getenv("PRIVATE_KEY_ID"),
+    "private_key": os.getenv("PRIVATE_KEY"),
+    "client_email": os.getenv("CLIENT_EMAIL"),
+    "client_id": os.getenv("CLIENT_ID"),
+    "auth_uri": os.getenv("AUTH_URI"),
+    "token_uri": os.getenv("TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.getenv("CLIENT_X509_CERT_URL")
 })
