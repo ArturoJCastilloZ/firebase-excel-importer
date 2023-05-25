e282929e7e811bcc49114bdac8f562b75d907ce1
ArturoJCastilloZ <castillo.arturo93@hotmail.com>
2023-05-25 09:37:02 -0600

    Cambios de nombre en las variables

--- api/config/firebase_config.py
+++ api/config/firebase_config.py
@@ -4,20 +4,20 @@ import os
 # Carga las variables de entorno para las credenciales del archivo .env
 load_dotenv()
 
-PATH_FILE_DATA = 'data.xlsx'
-DESTINATION_FILE = "./data.xlsx"
-BLOB_NAME = os.getenv('blob')
-BUCKET_NAME = os.getenv("bucket")
+PATH_FILE_DATA = os.getenv("PATH_FILE")
+DESTINATION_FILE = os.getenv("DESTINATION")
+BLOB_NAME = os.getenv("BLOB")
+BUCKET_NAME = os.getenv("BUCKET")
 
 CREDENTIAL_JSON = ({
-    "type": os.getenv("type"),
-    "project_id": os.getenv("project_id"),
-    "private_key_id": os.getenv("private_key_id"),
-    "private_key": os.getenv("private_key"),
-    "client_email": os.getenv("client_email"),
-    "client_id": os.getenv("client_id"),
-    "auth_uri": os.getenv("auth_uri"),
-    "token_uri": os.getenv("token_uri"),
-    "auth_provider_x509_cert_url": os.getenv("auth_provider_x509_cert_url"),
-    "client_x509_cert_url": os.getenv("client_x509_cert_url")
+    "type": os.getenv("TYPE"),
+    "project_id": os.getenv("PROJECT_ID"),
+    "private_key_id": os.getenv("PRIVATE_KEY_ID"),
+    "private_key": os.getenv("PRIVATE_KEY"),
+    "client_email": os.getenv("CLIENT_EMAIL"),
+    "client_id": os.getenv("CLIENT_ID"),
+    "auth_uri": os.getenv("AUTH_URI"),
+    "token_uri": os.getenv("TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.getenv("CLIENT_X509_CERT_URL")
 })
